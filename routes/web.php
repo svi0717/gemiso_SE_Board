1291d811615add689d888769b68a1a034050562a
설지섭 <twt12375@nate.com>
2024-08-28 13:35:12 +0900

    게시판 테스트 초기 버전 커밋

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('testboard');
+});
+
+Route::get('/schedule', function () {
+    return view('schedule');
+});
+
+Route::get('/editboard', function () {
+    return view('editboard');
+});
+
+Route::get('/deleteboard', function () {
+    return view('deleteboard');
+});
+
+Route::get('/buttontest', function () {
+    return view('buttontest');
 });
