3a56d5ab0dc6df4aef67b20d8961b602d633eea6
신준수 <tlswnstn0191@naver.com>
2024-09-02 10:28:25 +0900

    [진행중] 로그인 창 헤더

--- resources/views/layouts/loginHeader.blade.php
+++ resources/views/layouts/loginHeader.blade.php
@@ -4,31 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header and Sidebar Example</title>
-    
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- SweetAlert2 라이브러리 추가 -->
+
     <link rel="stylesheet" href="/css/header.css">
     <script src="{{ asset('js/buttons.js') }}" defer></script>
-    
+
     <style>
         .btn-custom {
-            font-size: 20px; /* 버튼 글씨 크기 조정 */
-            padding: 10px 20px; /* 버튼 패딩 조정 */
-            width: 60%; /* 버튼의 폭을 컨테이너에 맞춤 */
-            text-align: center; /* 버튼 텍스트 가운데 정렬 */
+            font-size: 20px;
+            padding: 10px 20px;
+            width: 60%;
+            text-align: center;
         }
 
         .sidebar {
-            margin-top: 180px; 
+            margin-top: 180px;
         }
 
         .content {
-            margin-top: 20px; /* 콘텐츠를 아래로 내리는 여백 조정 */
+            margin-top: 20px;
         }
 
         .row {
-            margin-left: 0; /* 좌우 여백 제거 */
-            margin-right: 0; /* 좌우 여백 제거 */
+            margin-left: 0;
+            margin-right: 0;
         }
 
         .sidebar div {
@@ -39,12 +40,24 @@
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="#">Gemiso SE</a>
+            <a class="navbar-brand" href="{{ auth()->check() ? route('board.list') : '/' }}">Gemiso SE</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ml-auto">
+                    @auth
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="#">홍길동</a>
+                        <a class="nav-link text-white" href="#">{{ Auth::user()->name }}</a>
                     </li>
+                    <li class="nav-item">
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                        <a class="nav-link text-white" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">로그아웃</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('login.form') }}">로그인</a>
+                    </li>
+                @endauth
                 </ul>
             </div>
         </nav>
